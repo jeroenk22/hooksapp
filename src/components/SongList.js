bfd2f1d73daadf308a23fe9ebe039058b9ad6184
Jeroen <jeroenkrajenbrink@gmail.com>
2020-07-01 21:26:50 +0200

    Added uuid for unique keys

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { v1 as uuidv1 } from 'uuid';
 
 const SongList = () => {
+  const [songs, setSongs] = useState([
+    { id: 1, title: 'almost home' },
+    { id: 2, title: 'memory gospel' },
+    { id: 3, title: 'this wild darkness' },
+  ]);
+
+  const addSong = () => {
+    setSongs([...songs, { id: uuidv1(), title: 'new song' }]);
+  };
+
   return (
     <div className='song-list'>
       <ul>
-        <li>this wild darkness</li>
-        <li>memory gospel</li>
+        {songs.map((song) => {
+          return <li key={song.id}>{song.title}</li>;
+        })}
       </ul>
+      <button onClick={addSong}>Add a song</button>
     </div>
   );
 };
